2dc282b18fe03245b6056d1d610711af75220f48
Adya <adya.glu@gmail.com>
2016-11-07 00:27:45 +0200

    Missed Save Change command.

---
+++ Skyrim Mods Tracker/ViewModels/Commands/SaveChangesCommand.cs
@@ -0,0 +1,23 @@
+﻿using SMT.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.ViewModels.Commands
+{
+    class SaveChangesCommand : BaseCommand<bool>
+    {
+        public override bool CanExecute(bool parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(bool parameter)
+        {
+            ModsManager.NormalizeMods();
+            StorageManager.Sync();
+        }
+    }
+}

5fb573ab376a2cfe1ff4159d831db57af36b9bf6
Adya <adya.glu@gmail.com>
2016-11-07 00:27:53 +0200

    Missed Save Change command.

---
+++ Skyrim Mods Tracker/ViewModels/ServersViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SMT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.ViewModels
+{
+    class ServersViewModel : BaseViewModel
+    {
+
+        public ServersViewModel(ICollection<Server> servers)
+        {
+
+        }
+    }
+}
